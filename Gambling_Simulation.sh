480b9b68e80b609765251bd911825c9f54974fa5
Kalyani <kalyanirane19@gmail.com>
2020-01-27 15:45:57 +0530

    ADDED : Check Gambler is Win or Loose

--- Gambling_Simulation.sh
+++ Gambling_Simulation.sh
@@ -2,7 +2,15 @@
 
 echo "Welcome Gambling Simulation Problem"
 
-
+#Declaring and Initializing Constants
 STAKE_OF_PER_DAY=100
 PER_BET=1
 
+#Check Gambler is Win or Loose
+
+	if [[ $((RANDOM%2)) -eq 1 ]]
+	then
+			echo "Win..."
+	else
+			echo "Loose..."
+	fi
