e3fa0d43da399f2a0d7d1af2ca94d1f182d696cc
Kalyani <kalyanirane19@gmail.com>
2020-01-27 17:05:52 +0530

    ADDED : Function to check gambler win or loose upto limit

--- Gambling_Simulation.sh
+++ Gambling_Simulation.sh
@@ -6,11 +6,28 @@ echo "Welcome Gambling Simulation Problem"
 STAKE_OF_PER_DAY=100
 PER_BET=1
 
-#Check Gambler is Win or Loose
+#Calculate 50% of stake
+percent=$((STAKE_OF_PER_DAY * 50 / 100))
 
-	if [[ $((RANDOM%2)) -eq 1 ]]
-	then
-			echo "Win..."
-	else
-			echo "Loose..."
-	fi
+max_cash=$((STAKE_OF_PER_DAY + percent))
+min_cash=$((STAKE_OF_PER_DAY - percent))
+cash=$STAKE_OF_PER_DAY
+
+#Check Gambler is Win or Loose
+function DailyLimit()
+{
+	while [ $cash -gt $min_cash ] && [ $cash -lt $max_cash ]
+	do
+		if [[ $((RANDOM%2)) -eq 1 ]]
+		then
+				((cash++))
+				echo "Win...$cash"
+		else
+				((cash--))
+				echo "Loose...$cash"
+		fi
+	done
+}
+
+DailyLimit
+ echo "Total cash of the day: $cash"
